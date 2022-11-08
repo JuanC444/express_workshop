4e8db0a96663b39f1ac28fe5b1941309ec56b7b9
Juan Carlos Almaguer Alfaro <jcalmaguer1234@gmail.com>
2022-11-08 16:43:47 -0600

    Actividad 8. Middleware para 404

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const pokemon = require('./routes/pokemon');
 
 //FUNCIONES PARA EXPRESAR PETICIONES
 app.use(morgan('dev'));
-app.use(express.json()); //.use - AÑADE MIDDLEWARES PARA PETICIONES
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 /* VERBOS HTTP - MODELO REST
 GET - OBTENER RECURSOS
@@ -17,10 +17,14 @@ DELETE - BORRAR UN RECURSO
 */
 
 app.get("/", (req, res, next) =>{
-    return res.status(200).send("Bienvenido al pokedex");
+    return res.status(200).json({code: 1, message: "Bienvenido al Pokédex"})
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req,res,next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"})
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("server is running...");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,7 +8,7 @@ pokemon.post("/", (req, res, next)=>{
 
 pokemon.get("/", async(req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1,message: pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async(req, res, next) =>{
@@ -16,8 +16,8 @@ pokemon.get('/:id([0-9]{1,3})', async(req, res, next) =>{
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + (id));
     console.log(pkmn);
     (id >= 1 && id <= 722) ? 
-    res.status(200).json(pkmn):
-    res.status(404).send("Pokemon no encontrado");
+    res.status(200).json({code: 1, message: pkmn}):
+    res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async(req, res, next)=>{
@@ -28,8 +28,8 @@ pokemon.get('/:name([A-Za-z]+)', async(req, res, next)=>{
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = " + "'" + (name) + "'");
         console.log(pkmn);
         (pkmn.length > 0) ? 
-        res.status(200).send(pkmn) : 
-        res.status(404).send("Pokemon no encontrado");
+        res.status(200).json({code: 1, message: pkmn}):
+        res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 });
 
 module.exports = pokemon;
