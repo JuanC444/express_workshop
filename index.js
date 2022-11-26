f18f96a82f5db6addc66c979d283f3e80b9939db
Juan Carlos Almaguer Alfaro <jcalmaguer1234@gmail.com>
2022-11-25 23:53:28 -0600

    14. Conectando con UI

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
 //FUNCIONES PARA EXPRESAR PETICIONES
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
--- routes/user.js
+++ routes/user.js
@@ -32,7 +32,7 @@ user.post("/login", async(req, res, next)=>{
             }, "debugkey");
             return res.status(200).json({code: 200, message:token});
         }else{
-            return res.status(401).json({code:401, message:"Usuario y/o contraseña incorrectos"});
+            return res.status(200).json({code:200, message:"Usuario y/o contraseña incorrectos"});
         }
     }
     return res.status(500).json({code: 500, message:"Campos Incompletos"});
